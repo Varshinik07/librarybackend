a45d3862ac901fe61be055fe9549cb447277a993
Varshini <varshuk01@gmail.com>
2022-10-19 20:23:58 +0530

    message

---
+++ src/main/java/com/nestdigital/librarybackendapp/LibraryBackendAppApplication.java
@@ -0,0 +1,13 @@
+package com.nestdigital.librarybackendapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryBackendAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryBackendAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nestdigital/librarybackendapp/LibraryBackendAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nestdigital.librarybackendapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryBackendAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
